209a3e8df56bad4b71ca9bbf120840cd8855d4f4
Andrew Taylor <andrew@andrewt.net>
2024-06-29 20:19:41 +0100

    Change styling to CSS rather than style tags

--- main.ts
+++ main.ts
@@ -58,58 +58,33 @@ export default class CrosswordPlugin extends Plugin {
 			console.log(xml);
 		} catch(e) {
 			el.innerHTML = "";
-			child(el, "h1", "Error parsing XML");
-			child(el, "pre", e.stack);
+			child(el, "h1", "cxw-error", "Error parsing XML");
+			child(el, "pre", "cxw-error", e.stack);
 		} 
 	}
 
 	renderGrid(grid: any): HTMLElement {
-		const table = document.createElement('table'),
+		const table = newEl('table', 'cxw-grid'),
 			tbody = child(table, "tbody"),
 			w = parseInt(grid.$width, 10),
 			h = parseInt(grid.$height, 10),
 			cells: Record<string, Record<string, HTMLElement>> = {};
 		for (let y = 1; y <= h; ++y) {
-			const rowEl = child(tbody, "tr");
+			const rowEl = child(tbody, "tr", "cxw-cell");
 			const row: Record<string, HTMLElement> = {};
 			cells[y] = row;
-			for (let x = 1; x <= w; ++x) {
+			for (let x = 1; x <= w; ++x)
 				row[x] = child(rowEl, "td");
-				row[x].style.background = `white`;
-				row[x].style.border = `1px solid black`;
-				row[x].style.height =
-				row[x].style.minHeight =
-				row[x].style.maxHeight =
-				row[x].style.width =
-				row[x].style.minWidth =
-				row[x].style.maxWidth = "2.5em";
-				row[x].style.padding = "0";
-				row[x].style.position = "relative";
-			}
 		}
 		for (const cell of grid.cell) {
 			const el = cells[cell.$y][cell.$x];
-			if (cell.$type == 'block') {
-				el.style.background = 'black';
-			}
-			if (cell.$number) {
-				const num = child(el, "span", cell.$number);
-				num.style.fontSize = "0.75em";
-				num.style.position = "absolute";
-				num.style.top = "0.25em";
-				num.style.left = "0.25em";
-			}
-			if (cell.$solution) {
-				const sol = child(el, "div", cell.$solution);
-				sol.style.fontSize = "1.5em";
-				sol.style.position = "absolute";
-				sol.style.top = "0.25em";
-				sol.style.left = "0.25em";
-				sol.style.right = "0.25em";
-				sol.style.textAlign = "center";
-			}
+			if (cell.$type == 'block')
+				el.classList.add('cxw-block');
+			if (cell.$number)
+				child(el, "span", "cxw-number", cell.$number);
+			if (cell.$solution)
+				child(el, "div", "cxw-letter", cell.$solution);
 		}
-		table.style.borderCollapse = `collapse`;
 		return table;
 	}
 
@@ -119,33 +94,33 @@ export default class CrosswordPlugin extends Plugin {
 		const el = document.createElement('div');
 		for (const { title, clue } of clues) {
 			const e = child(el, "div");
-			child(e, "h2", title.b ?? title);
+			child(e, "h2", "cxw-clue-list-title", title.b ?? title);
 			const ol = child(e, "ol");
 			for (const c of clue) {
-				const li = child(ol, "li");
-				li.style.display = "flex";
-				li.style.marginLeft = "0";
-				const n = child(li, "div", c.$number + '.');
-				n.style.flex = "3em 0 0";
-				const t = child(li, "div");
-				t.style.flex = "5em 1 0";
-				const main = child(t, "div");
-				child(main, "span").innerHTML = c["#text"];
-				child(main, "span", ` (${c.$format})`);
-				const citation = child(t, "div",
+				const li = child(ol, "li", "cxw-clue");
+				child(li, "div", "cxw-clue-number", c.$number + '.');
+				const t = child(li, "div", "cxw-clue-text");
+				const main = child(t, "div", "cxw-clue-body");
+				child(main, "span", "cwx-clue-body-text").innerHTML = c["#text"];
+				child(main, "span", "cxw-clue-format" ` (${c.$format})`);
+				child(t, "div", "cxw-clue-citation",
 					`${c["$citation"]} 🡒 ${wordsById[c.$word].$solution.toUpperCase()}`
 				);
-				citation.style.fontSize = "0.75em";
-				citation.style.color = '#444';
 			}
 		}
 		return el;
 	}
 }
 
-function child(parent: HTMLElement, tag: string, content?: string) {
+function newEl(tag: string, className?: string content?: string) {
 	const h = document.createElement(tag);
+	if (className) h.classList.add(className);
 	if (content) h.innerText = content;
+	return h;
+}
+
+function child(parent: HTMLElement, tag: string, className?: string, content?: string) {
+	const h = newEl(tag, className, content);
 	parent.appendChild(h);
 	return h;
 }
